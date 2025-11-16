49516087030ec10f530ae784f9eeeca39e3b2d62
Shivam-meena-art <manthanmeena2006@gmail.com>
2025-11-17 02:33:39 +0530

    page not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound(){
+    return (
+        <div>
+            <h1>Page Not found</h1>
+            <p>Could Not find requested resources</p>
+        </div>
+    );
+}
