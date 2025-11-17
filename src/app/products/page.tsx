46f1dc6cfe506376a4aab0179b86a238ac5a5a0b
Shivam-meena-art <manthanmeena2006@gmail.com>
2025-11-17 15:34:09 +0530

    Linking components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+export default function ProductsList(){
+
+    return (
+        <>
+          <Link href="/">Home</Link>
+
+     
+          <h2>
+            <Link href="/product/1">Product 1</Link>
+          </h2>
+          <h2>
+            <Link href ="/product/2">Product 2</Link>
+          </h2>
+          <h2>
+            <Link href ="/product/3">Product 3</Link>
+          </h2>
+              
+        </>
+    );
+}
